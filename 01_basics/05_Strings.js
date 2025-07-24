395eaa00924e6faa3608c4488e92ae51f6623639
Ashutosh-kumar-Choudhary <ashutoshchoudhary0310@gmail.com>
2025-07-24 09:24:21 +0000

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,171 @@
+const name ="AShutosh"
+const repocount = 2
+
+console.log(name + repocount);
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+// anchor
+// : 
+// ƒ anchor()
+// at
+// : 
+// ƒ at()
+// big
+// : 
+// ƒ big()
+// blink
+// : 
+// ƒ blink()
+// bold
+// : 
+// ƒ bold()
+// charAt
+// : 
+// ƒ charAt()
+// charCodeAt
+// : 
+// ƒ charCodeAt()
+// codePointAt
+// : 
+// ƒ codePointAt()
+// concat
+// : 
+// ƒ concat()
+// constructor
+// : 
+// ƒ String()
+// endsWith
+// : 
+// ƒ endsWith()
+// fixed
+// : 
+// ƒ fixed()
+// fontcolor
+// : 
+// ƒ fontcolor()
+// fontsize
+// : 
+// ƒ fontsize()
+// includes
+// : 
+// ƒ includes()
+// indexOf
+// : 
+// ƒ indexOf()
+// isWellFormed
+// : 
+// ƒ isWellFormed()
+// italics
+// : 
+// ƒ italics()
+// lastIndexOf
+// : 
+// ƒ lastIndexOf()
+// length
+// : 
+// 0
+// link
+// : 
+// ƒ link()
+// localeCompare
+// : 
+// ƒ localeCompare()
+// match
+// : 
+// ƒ match()
+// matchAll
+// : 
+// ƒ matchAll()
+// normalize
+// : 
+// ƒ normalize()
+// padEnd
+// : 
+// ƒ padEnd()
+// padStart
+// : 
+// ƒ padStart()
+// repeat
+// : 
+// ƒ repeat()
+// replace
+// : 
+// ƒ replace()
+// replaceAll
+// : 
+// ƒ replaceAll()
+// search
+// : 
+// ƒ search()
+// slice
+// : 
+// ƒ slice()
+// small
+// : 
+// ƒ small()
+// split
+// : 
+// ƒ split()
+// startsWith
+// : 
+// ƒ startsWith()
+// strike
+// : 
+// ƒ strike()
+// sub
+// : 
+// ƒ sub()
+// substr
+// : 
+// ƒ substr()
+// substring
+// : 
+// ƒ substring()
+// sup
+// : 
+// ƒ sup()
+// toLocaleLowerCase
+// : 
+// ƒ toLocaleLowerCase()
+// toLocaleUpperCase
+// : 
+// ƒ toLocaleUpperCase()
+// toLowerCase
+// : 
+// ƒ toLowerCase()
+// toString
+// : 
+// ƒ toString()
+// toUpperCase
+// : 
+// ƒ toUpperCase()
+// toWellFormed
+// : 
+// ƒ toWellFormed()
+// trim
+// : 
+// ƒ trim()
+// trimEnd
+// : 
+// ƒ trimEnd()
+// trimLeft
+// : 
+// ƒ trimStart()
+// trimRight
+// : 
+// ƒ trimEnd()
+// trimStart
+// : 
+// ƒ trimStart()
+// valueOf
+// : 
+// ƒ valueOf()
+// Symbol(Symbol.iterator
+ const gamename = new String("Game-changer")
+ console.log(gamename[0]);
+ console.log(gamename[1]);
+ console.log(gamename[2]);
+ console.log(gamename[3]);
+ console.log(gamename.__proto__);
+ console.log(gamename.substring(0,5));
+ const anotherstring =gamename.slice(-8,4)
+ console.log(anotherstring);
