9aa3a32134040f1eaf8a8cb3a643e5ce279b46eb
Ashutosh-kumar-Choudhary <ashutoshchoudhary0310@gmail.com>
2025-07-24 10:06:31 +0000

    nums&maths

---
+++ 01_basics/06nums and math.js
@@ -0,0 +1,42 @@
+const score=400
+console.log(score);
+
+
+ 
+const balance=new Number(100)
+console.log(balance);
+// constructor
+// : 
+// ƒ Number()
+// toExponential
+// : 
+// ƒ toExponential()
+// toFixed
+// : 
+// ƒ toFixed()
+// toLocaleString
+// : 
+// ƒ toLocaleString()
+// toPrecision
+// : 
+// ƒ toPrecision()
+// toString
+// : 
+// ƒ toString()
+// valueOf
+// : 
+// ƒ valueOf()
+
+// *****************************Maths*******************************************//
+console.log(Math);
+console.log(Math.random());
+console.log(Math.random()*10+1);
+const min = 10
+const max=20
+
+
+
+
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
