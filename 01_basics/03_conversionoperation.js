77d0e1be8c8168be6e5a7421bf8c0b026700e4b6
Ashutosh-kumar-Choudhary <ashutoshchoudhary0310@gmail.com>
2025-07-24 07:34:21 +0000

    added some extra info.

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -10,10 +10,10 @@
 // let booleanIsloggedIn = Boolean(isloggedIn);
 // console.log(booleanIsloggedIn);
 
- let somenumber = 33
- let stringNumber = String(somenumber)
- console.log(stringNumber)
- console.log(typeof stringNumber)
+//  let somenumber = 33
+//  let stringNumber = String(somenumber)
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber)
 
 
 // console.log(typeof score)
@@ -24,3 +24,25 @@
 // "33"=> 33
 //"33abc"=>Nan
 // true =>1; false=>0
+//**********************Operations**************//
+/*let value = -3
+let negvalue = - value
+console.log(negvalue);
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**5)
+console.log(2/3)
+console.log(2%2)
+*/
+let str1="Hello"
+let str2 ="Ashu"
+console.log(str1+str2);
+console.log("1"+2+2);// important case
+console.log(1+2+"2");
+console.log( +true );
+
+//link tp study "https://tc39.es/ecma262/multipage/abstract-operations.html"
+
+
+// postfix , prefix operations :https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
