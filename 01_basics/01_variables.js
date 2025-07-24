73565d3cae7aa75ba048a5d9a77bc38934143b80
Ashutosh-kumar-Choudhary <ashutoshchoudhary0310@gmail.com>
2025-07-24 06:21:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const account_id = 144553
+let accountEmail = "ashutosh@google.com"
+var accountPassword = "12345"
+accountCity ="jaipur"
+let accountstate;
+accountEmail = "satyam@google.com"
+ accountPassword = "54321"
+ accountCity ="Burari"
+
+ 
+//account_id = 2  //not allowed
+console.log(account_id)
+console.table([account_id,accountEmail,accountPassword,accountCity,accountstate])
+/* Prefer not to use var because of issue in block scope and functional scope */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+// alert(3+2) //we are using nodejs , not browser
+// console.log(3+3); console.log("Ashutosh")
+// tc39.es and mdn  are the helpful documentation site
+let name ="Ashutosh"
+let age = 18
+let isloggedIn= false
+let age2 = null
+let age3 = undefined
+
+//number => 2 to power 53
+//bigint 
+//string =>""
+// boolean => true/false
+//null => standalone value
+// undefined =>
+    // symbol =>unique
+// object
+
+console.log(typeof"Ashutosh",typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
