b6b24ef55b4620bf3ae4a77afddd9c8aa87d4d01
Ashutosh-kumar-Choudhary <ashutoshchoudhary0310@gmail.com>
2025-07-24 08:06:06 +0000

    Dataype summary

---
+++ 01_basics/Datatype summary.js
@@ -0,0 +1,18 @@
+//Primitive
+// 7 types : String , Number, Boolean ,null , Undefined,SymbolBigInt\
+
+// Reference(Non Primitive)
+// Array , Objects, Functions 
+const heroes = ["shaktiman", "Nagraj", "doga"];
+
+let myobj = {
+    name: "Ashutosh",
+    age: 22
+};
+
+const myfunction = function() {
+    console.log("Hello world");
+};
+
+console.log(typeof heroes); 
+// https://262.ecma-international.org/5.1/#sec-11.4.3
