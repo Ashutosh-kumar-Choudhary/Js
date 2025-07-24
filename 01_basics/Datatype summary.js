8a13af860a80fab14496be3fa1e9e438ac4639e4
Ashutosh-kumar-Choudhary <ashutoshchoudhary0310@gmail.com>
2025-07-24 08:23:33 +0000

    Datatype summary

--- 01_basics/Datatype summary.js
+++ 01_basics/Datatype summary.js
@@ -16,3 +16,23 @@ const myfunction = function() {
 
 console.log(typeof heroes); 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//****************************************************** *//
+// stack (Primitive) , heap(Non -Primitive)
+let myinstaname = "Ashuhumaii"
+ let anothername = myinstaname
+ anothername ="Chai sutta bar"
+ console.log(myinstaname);
+ console.log(anothername);
+  let user ={
+    email :"user@google.com",
+    upi :"user@ibl"
+  }
+  
+console.log(user.email);  
+let userone = user 
+   userone.email= "someone@gmail.com"
+
+console.log(user.email); 
+console.log(userone.email); 
