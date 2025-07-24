d5320b7e36ac03afae831cbadc759221533fcb9a
Ashutosh-kumar-Choudhary <ashutoshchoudhary0310@gmail.com>
2025-07-24 06:56:39 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+// let score =33
+// console.log(typeof score)
+// console.log(typeof(score))
+//let score ="33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score ="Ashutosh"
+// let isloggedIn = 1;
+// let booleanIsloggedIn = Boolean(isloggedIn);
+// console.log(booleanIsloggedIn);
+
+ let somenumber = 33
+ let stringNumber = String(somenumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
+
+
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)//not a number still give the datatype as a number
+// "33"=> 33
+//"33abc"=>Nan
+// true =>1; false=>0
